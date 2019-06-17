8e3e3385f8c11a280d159ce2befee13aba5eda3d
Alex Deane <alexrdeane@hotmail.com>
2019-06-17 20:18:01 +1000

    adding colliders

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -9,20 +9,13 @@ using UnityEngine.SceneManagement;
 
 public class Damage : MonoBehaviour
 {
-    public Slider oppoHealth;
+    public Slider Health;
     //slider for Health can be 
     public Collider hitCol;
     /// <summary>
     /// //players hit colider yo
     /// </summary>
-    public Collider enmCol;
-    //ennemy ditectiontions yeah
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //i dont know why this is here, 
-    }
     /// <summary>
     /// /so like updates check the health if there is 
     ///no health, then do a thing
@@ -31,7 +24,7 @@ public class Damage : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if(oppoHealth.value <= 0)
+        if(Health.value <= 0)
         {
             Debug.Log("death");
             //retsart the scene can make do whatever, just a thingy to show it works
@@ -40,13 +33,25 @@ public class Damage : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter(Collider enmCol)
+    void OnTriggerEnter(Collider hitCol)
     {
         //make suure the right tag hits it right thingy 
         //using same way copy nd paste can be appled to various coldiers
-        if (gameObject.tag == "Oppo_damage_Tag")
+        if (gameObject.tag == "punchCol")
         {
-            oppoHealth.value -= 1;
+            Health.value -= 3;
+            ///do some damage, hitbar slider will do down
+            Debug.Log("hiit");
+        }
+        if (gameObject.tag == "kickCol")
+        {
+            Health.value -= 5;
+            ///do some damage, hitbar slider will do down
+            Debug.Log("hiit");
+        }
+        if (gameObject.tag == "kickCol2")
+        {
+            Health.value -= 10;
             ///do some damage, hitbar slider will do down
             Debug.Log("hiit");
         }
